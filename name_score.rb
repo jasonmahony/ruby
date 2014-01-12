37c4792a9566e2a646e5824002922f75aef49828
Jason Mahony <jason@verticalresponse.com>
2014-01-12 13:59:18 -0800

    Added name_score riddle game

---
+++ name_score.rb
@@ -0,0 +1,26 @@
+#!/Users/jason/.rvm/rubies/ruby-1.9.3-p429/bin/ruby
+
+name = ""
+
+def say_hello(name, health)
+  "Hi, I'm #{name.capitalize} with a health of #{health}"
+end
+
+def calc_health(f_letter)
+  letters = ('a'..'z').to_a
+  numbers = (1..26).to_a
+  hash = Hash[letters.zip numbers]
+  hash[f_letter]  
+end
+
+while name != "quit"
+  puts "Enter the name and hit enter. Type 'quit' to exit"
+  name = gets.chomp
+  if name == "quit"
+    break
+  else
+  first = name[0].downcase
+  puts say_hello(name, calc_health(first))
+  end
+end
+

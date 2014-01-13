25fc1d3901d2d90ac8f35ac991a151fafb02c8c4
Jason Mahony <jason@verticalresponse.com>
2014-01-12 23:22:28 -0800

    Riddle game flow improvement

--- riddle.rb
+++ riddle.rb
@@ -1,42 +1,46 @@
 #!/Users/jason/.rvm/rubies/ruby-1.9.3-p429/bin/ruby
 
-text = ""
+text = nil
 
 def give_score(text, score)
   "Your score is #{score}."
 end
 
-def calc_score(f_letter)
+def calc_score(text)
+  f_char = text[0].downcase
   letters = ('a'..'z').to_a
   numbers = (1..26).to_a
   hash = Hash[letters.zip numbers]
-  hash[f_letter]  
+  hash[f_char]  
+end
+
+def generate(size = 15)
+  chars = [*('A'..'Z'), *('a'..'z')]
+  (0...size).map{ chars.to_a[rand(chars.size)] }.join
 end
 
 puts "^^^^^^^^^^^^^^^^^^^^^^^\nWelcome to \"The Riddle\"\n^^^^^^^^^^^^^^^^^^^^^^^"
 
-while text != "quit"
-  puts "\n\nEnter some text and hit enter. Type 'quit' or hit Enter to exit"
+while text != ""
+  puts "\nWrite some text. Hit Enter (and nothing else) to exit."
   text = gets.chomp
-  if text == "quit"
-    break
-  elsif text == ""
+  if text == ""
     break
   else
-    first = text[0].downcase
     puts "\n\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^  #{give_score(text, calc_score(first))}  ^^^^^^"
+    puts "^^^^^^  #{give_score(text, calc_score(text))}  ^^^^^^"
     puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-    puts "\nCan you guess why the reason for your score?"
-    puts "\nType 'yes' if you want to try and guess the riddle. To get more clues just hit Enter"
+    puts "\nType 'yes' to solve the riddle. Hit Enter to continue."
+    rand_text = generate
     ready = gets.chomp
     if ready  == "yes"
-      puts "\nWhat is the score of the following text 'wzulskdjk kdsfkj zomem alajkd'\?"
+      score = calc_score(rand_text) 
+      puts "\nWhat is the score of the following text '#{rand_text}'\?"
       answer = gets.chomp.to_i
-      if answer == 23
+      if answer == calc_score(rand_text)
         puts "\nNice work! You win!"
       else
-        puts "\nSooooory"
+        puts "\nSooooory..."
       end
       break
     end

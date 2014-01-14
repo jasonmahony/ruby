191f32a22f297acb9c73c23b41faaac3b0f41e95
Jason Mahony <jason@verticalresponse.com>
2014-01-14 03:59:42 -0800

    refactoring

--- riddle.rb
+++ riddle.rb
@@ -1,50 +1,59 @@
-#!/Users/jason/.rvm/rubies/ruby-1.9.3-p429/bin/ruby
+#!/usr/bin/env ruby
 
-text = nil
+class Text
 
-def give_score(text, score)
-  "Your score is #{score}."
+  def initialize(text)
+    @text = text
+    @coder = Hash[('a'..'z').to_a.zip (1..26).to_a]
+  end
+
+  def calc_score
+    f_char = @text[0].downcase
+    @coder[f_char]  
+  end
+
+  def print
+    @text
+  end
+
 end
-
-def calc_score(text)
-  f_char = text[0].downcase
-  letters = ('a'..'z').to_a
-  numbers = (1..26).to_a
-  hash = Hash[letters.zip numbers]
-  hash[f_char]  
+ 
+def formatting(input)
+  puts "^".center(60, "^")
+  puts input.center(60, "^")
+  puts "^".center(60, "^")
 end
-
+  
 def generate(size = 15)
   chars = [*('A'..'Z'), *('a'..'z')]
   (0...size).map{ chars.to_a[rand(chars.size)] }.join
 end
 
-puts "^^^^^^^^^^^^^^^^^^^^^^^\nWelcome to \"The Riddle\"\n^^^^^^^^^^^^^^^^^^^^^^^"
+puts formatting("Welcome to \"The Riddle\"")
 
-while text != ""
-  puts "\nWrite some text to play. Hit Enter (and nothing else) to exit.\n\n"
-  text = gets.chomp
-  if text == ""
+loop do
+  puts "Write some text to play. Hit 'q' to exit.\n"
+  input = Text.new(gets.chomp)
+  if input.print == "q"
     break
   else
-    puts "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-    puts "^^^^^^  #{give_score(text, calc_score(text))}  ^^^^^"
-    puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-    puts "\nType 'yes' to solve the riddle of how your score is generated. Hit Enter to keep playing."
-    rand_text = generate
-    ready = gets.chomp
-    if ready  == "yes"
-      score = calc_score(rand_text) 
-      puts "\nWhat is the score of the following text '#{rand_text}'\?"
+    puts formatting("Your score is #{input.calc_score}.")
+    puts "Can you guess why? Hit 'y' to solve!"
+    puts "\nHit Enter to keep playing."
+    rand_text = Text.new(generate)
+    ready = Text.new(gets.chomp)
+    if ready.print  == "y"
+      score = input.calc_score
+      puts "\nWhat is the score of the following text '#{rand_text.print}'\?"
       answer = gets.chomp.to_i
-      if answer == calc_score(rand_text)
-        puts "\nNice work!! You win!"
+      if answer == rand_text.calc_score
+        puts formatting("Nice work!! You win!")
       else
         puts "\nSooooorry..."
       end
       break
+    else
+      puts "\nOK, still trying..."
     end
-    puts "\nOK, still trying..."
   end
 end
-

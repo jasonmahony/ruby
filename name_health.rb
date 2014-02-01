d47c428d3ebb4b07f6ea7016629676c577c3eb08
Jason Mahony <jason.mahony@gmail.com>
2014-02-01 08:50:11 -0800

    Update name_health.rb
    using a class to assist with output

--- name_health.rb
+++ name_health.rb
@@ -1,26 +1,18 @@
 #!/usr/bin/env ruby
 
-name = ""
+class Lover
 
-def say_hello(name, health)
-  "Hi, I'm #{name.capitalize} with a health of #{health}"
-end
-
-def calc_health(f_letter)
-  letters = ('a'..'z').to_a
-  numbers = (1..26).to_a
-  hash = Hash[letters.zip numbers]
-  hash[f_letter]  
-end
-
-while name != "quit"
-  puts "Enter the name and hit enter. Type 'quit' to exit"
-  name = gets.chomp
-  if name == "quit"
-    break
-  else
-  first = name[0].downcase
-  puts say_hello(name, calc_health(first))
+  def initialize(name, health)
+    @name = name.downcase
+    @health = health
   end
+  
+  def to_s
+    "Hi, I'm #{@name.capitalize} with a score of #{@health}"
+  end
+
 end
 
+love = Lover.new("jUlIe", 10)
+
+puts love.to_s

9d7c05615c78cecb33ac6a62054de51da92a739f
Jason Mahony <jason@verticalresponse.com>
2014-07-02 11:54:05 -0700

    runoff ruby script mods

--- runoff.rb
+++ runoff.rb
@@ -2,9 +2,9 @@
 
 def runoff(voters)
   num = voters.length
-  count = {:dem => 0, :ind => 0, :rep => 0}   
+  count = {:dem => 0, :ind => 0, :rep => 0}
   voters.each do |vote|
-    case vote[0]
+    case vote.shift
     when :dem
       count[:dem] += 1
     when :rep
@@ -14,34 +14,19 @@ def runoff(voters)
     end
   end
   sorted_array = count.sort_by {|k, v| v}.reverse
-
-  if sorted_array[0][1] > num/2
+  if sorted_array[0][1] > num / 2
     return sorted_array[0][0]
-  else 
-    voters.each do |vote|
-      (num - 1).times do
-        case vote.shift
-        when :dem
-          count[:dem] += 1
-        when :rep
-          count[:rep] += 1
-        when :ind
-          count[:ind] += 1
-        end
-      end
-    end
-    if sorted_array[2][1] == sorted_array[0][1]
-      return nil
-    else
-      return sorted_array[1][0]
-    end
-  end   
+  elsif sorted_array[0][1] == sorted_array[2][1] 
+    return nil
+  else
+    runoff(voters)
+  end
 end
 
 voters = [[:dem, :ind, :rep],
           [:rep, :ind, :dem],
           [:ind, :dem, :rep],
-#          [:ind, :rep, :dem],
+          [:dem, :rep, :dem],
           [:ind, :rep, :dem]]
 
 puts runoff(voters)

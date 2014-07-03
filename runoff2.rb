6b683ffaeec895bee0cb5501eab818522dc2ba94
Jason Mahony <jason@verticalresponse.com>
2014-07-02 17:22:35 -0700

    adding work in progress scripts

--- runoff2.rb
+++ runoff2.rb
@@ -2,24 +2,17 @@
 
 def runoff(voters)
   num = voters.length
-#  count = {:dem => 0, :ind => 0, :rep => 0}
+  count = Hash[voters[0].map { |i| [i, 0] }]
   voters.each do |vote|
-    case vote.shift
-    when :dem
-      count[:dem] += 1
-    when :rep
-      count[:rep] += 1
-    when :ind
-      count[:ind] += 1
-    end
+    count[vote[0]] += 1
   end
   sorted_array = count.sort_by {|k, v| v}.reverse
   if sorted_array[0][1] > num / 2
     return sorted_array[0][0]
-  elsif sorted_array[0][1] == sorted_array[2][1]
-    return nil
   else
-    runoff(voters)
+    
+  
+  
   end
 end
 
---
+++ test.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+voters = [[:dem, :ind, :rep],
+          [:rep, :ind, :dem],
+          [:ind, :dem, :rep],
+          [:ind, :rep, :dem]]
+count = Hash[voters[0].map { |i| [i, 0] }]  
+num = voters.length
+
+voters.each do |vote|
+  num.times do
+    case vote.shift
+    when :dem
+      count[:dem] += 1
+    when :rep
+      count[:rep] += 1
+    when :ind
+      count[:ind] += 1
+    end
+  end
+end
+
+print count

67e977a7dc3b3678815d3a6d8ada327b54ec59a3
Jason Mahony <jason@verticalresponse.com>
2014-07-08 14:01:06 -0700

    renaming runoff2 script

--- runoff.rb
+++ runoff.rb
@@ -1,31 +1,38 @@
 #!/usr/bin/env ruby
 
 def runoff(voters)
-  num = voters.length
-#  count = {:dem => 0, :ind => 0, :rep => 0}
+  num_voters = voters.length
+  count = Hash[voters[0].map { |i| [i, 0] }]
+  num_candidates = count.length
   voters.each do |vote|
-    case vote.shift
-    when :dem
-      count[:dem] += 1
-    when :rep
-      count[:rep] += 1
-    when :ind
-      count[:ind] += 1
-    end
+    count[vote[0]] += 1
   end
-  sorted_array = count.sort_by {|k, v| v}.reverse
-  if sorted_array[0][1] > num / 2
-    return sorted_array[0][0]
-  elsif sorted_array[0][1] == sorted_array[2][1]
+  sorted_array = count.sort_by {|k, v| v}
+  if sorted_array[num_candidates - 1][1] > num_voters / 2
+    return sorted_array[num_candidates - 1][0]
+  elsif sorted_array[0][1] < sorted_array[num_candidates - 1][1]
+    if sorted_array[0][1] < sorted_array[1][1]
+  
+      new_voters = voters.map {|vote| vote - [sorted_array[0][0]] }
+      runoff(new_voters)
+    elsif sorted_array[0][1] = sorted_array[2][1]
+      
+      new_voters = voters.map {|vote| vote - [sorted_array[0][0], sorted_array[1][0], sorted_array[2][0]] }
+      runoff(new_voters) 
+      
+    elsif sorted_array[0][1] = sorted_array[1][1]
+      new_voters = voters.map {|vote| vote - [sorted_array[0][0], sorted_array[1][0]] }
+      runoff(new_voters)
+    end
+
+  else  
     return nil
-  else
-    runoff(voters)
   end
 end
 
 voters = [[:dem, :ind, :rep],
           [:rep, :ind, :dem],
-          [:ind, :dem, :rep],
+#          [:ind, :dem, :rep],
           [:dem, :rep, :dem],
           [:ind, :rep, :dem]]
 

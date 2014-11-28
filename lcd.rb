305bc5c30790eedb758066310587e335900ebcb3
Jason Mahony <jason@verticalresponse.com>
2014-11-27 22:34:55 -0800

    working on lcd program

--- lcd.rb
+++ lcd.rb
@@ -1,5 +1,38 @@
 #!/usr/bin/env ruby
 
+class Display
+  def initialize(integer, scale, lcds)
+    @integer = integer
+    @scale = scale
+    @lcds = lcds
+  end
+  
+  # Shows what we are going to do with the arguments
+  def showargs
+    puts "  The integer is #{@integer} with a scale of #{@scale}  ".center(60,'*')
+  end
+
+  # Display the LCD integer
+  def display
+    int_array = @integer.chars.map(&:to_i)
+    lcd_array = int_array.map { |int| @lcds[int] }
+    puts join(lcd_array, " ")
+#    new_d = Array.new
+#    digits.each do |d|
+#      new_d = d[1..-1] * @scale
+#    end
+#    puts new_d
+#   digits.map do |d|
+#     if d =~ /\|/
+#       @scale.times { puts d }
+#     else
+#       puts d
+#     end
+#   end  
+
+  end
+end
+
 # Fetching the arguments
 if ARGV.size == 2
   num = ARGV[0]
@@ -13,35 +46,6 @@ else
   puts " lcd <integer to scale> [ <scale> DEFAULT=1  ]"
   exit
 end
-
-class Display
-  def initialize(integer, scale, lcds=template)
-    @integer = lcd
-    @scale = scale
-    @digit = lcds
-  end
-  
-  # Shows what we are going to do with the arguments
-  def showargs
-    puts "  The integer is #{@lcd} with a scale of #{@size}  ".center(60,'*')
-  end
-
-  # Display the LCD integer
-  def display
-    lcds.each { |row| row[1, 1] = row[1, 1] * scale }
-    lcds.each do |row|
-      if row =~ /\|/
-        scale.times { puts row }
-      else
-        puts row
-      end
-    end
-  end
-end
-
-
-lcd = Display.new(num,scale).showargs
-lcd.display
 
 template = [
 
@@ -58,27 +62,27 @@ template = [
 ' '],
 
 [' - ',
-' |',
+'  |',
 ' - ',
 '| ',
 ' - '],
 
 [' - ',
-' |',
+'|',
 ' - ',
-' |',
+'|',
 ' - '],
 
 [' ',
 '| |',
 ' - ',
-' |',
+'  |',
 ' '],
 
 [' - ',
 '| ',
 ' - ',
-' |',
+'  |',
 ' - '],
 
 [' - ',
@@ -88,9 +92,9 @@ template = [
 ' - '],
 
 [' - ',
-' |',
-' ',
-' |',
+'  |',
+'   ',
+'  |',
 ' '],
 
 [' - ',
@@ -102,8 +106,12 @@ template = [
 [' - ',
 '| |',
 ' - ',
-' |',
+'  |',
 ' - ']
 
 ]
+
+lcd = Display.new(num, scale, template)
+lcd.showargs
+lcd.display
 

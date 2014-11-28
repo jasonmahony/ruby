6de959e222985101ef1f0f2b79edbbd0f965c0d8
Jason Mahony <jason@verticalresponse.com>
2014-11-28 08:25:00 -0800

    progress on lcd dispay

--- lcd.rb
+++ lcd.rb
@@ -3,33 +3,30 @@
 class Display
   def initialize(integer, scale, lcds)
     @integer = integer
-    @scale = scale
+    @scale = scale.to_i
     @lcds = lcds
   end
   
-  # Shows what we are going to do with the arguments
   def showargs
     puts "  The integer is #{@integer} with a scale of #{@scale}  ".center(60,'*')
   end
 
   # Display the LCD integer
   def display
-    int_array = @integer.chars.map(&:to_i)
-    lcd_array = int_array.map { |int| @lcds[int] }
-    puts join(lcd_array, " ")
-#    new_d = Array.new
-#    digits.each do |d|
-#      new_d = d[1..-1] * @scale
-#    end
-#    puts new_d
-#   digits.map do |d|
-#     if d =~ /\|/
-#       @scale.times { puts d }
-#     else
-#       puts d
-#     end
-#   end  
-
+#    int_array = @integer.chars.map(&:to_i)
+    digits = @integer.chars.map { |n| @lcds[n.to_i] }
+      digits.each do |digit|
+        digit.each do |row|
+          # Scale horizontally
+          row[1, 1] = row[1, 1] * @scale
+          # Scale vertically    
+          if row =~ /\|/
+            @scale.times { puts row }
+          else
+            puts row
+          end
+        end
+      end
   end
 end
 
@@ -43,7 +40,7 @@ elsif ARGV.size == 1
 # Printing message to explain the program if no arguments
 else
   puts "  This program accepts 1 or 2 arguments  ".center(60,'*')
-  puts " lcd <integer to scale> [ <scale> DEFAULT=1  ]"
+  puts " lcd <integer to scale> [ <scale> DEFAULT=1 ]"
   exit
 end
 
@@ -56,9 +53,9 @@ template = [
 ' - '],
 
 [' ',
-' |',
+'  |',
 ' ',
-' |',
+'  |',
 ' '],
 
 [' - ',
@@ -112,6 +109,5 @@ template = [
 ]
 
 lcd = Display.new(num, scale, template)
-lcd.showargs
 lcd.display
-
+lcd.showargs

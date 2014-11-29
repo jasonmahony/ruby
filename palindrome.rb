4cdf287f1e5c306f609f08d657b24bd32c8acde4
Jason Mahony <jason@verticalresponse.com>
2014-11-29 11:32:02 -0800

    commenting palindrome method

--- palindrome.rb
+++ palindrome.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
+
+# Find palindrome without using reverse method
 def palindrome(string)
+  # Strip out non-word and non-number characters
   string_array = string.gsub(/(\W|\d)/, "").downcase.split(//)
-  index = string_array.length
+  index = string_array.size
   reverse_array = []
   index.times { index -= 1; reverse_array = reverse_array.push(string_array[index])}
   
+  # Prints "yep" if input is palindrome or "nope" if not
   if string_array == reverse_array
     puts "yep"
   else

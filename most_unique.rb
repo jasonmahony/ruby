e53aba0e27ac1126765d787611b842467d06fc08
Jason Mahony <jason@verticalresponse.com>
2014-12-03 10:51:00 -0800

    commented most_unique script

--- most_unique.rb
+++ most_unique.rb
@@ -1,20 +1,28 @@
 #!/usr/bin/env ruby
 
+# Accepts unknown count of integers
 def find_suspect(*numbers)
-  sorted_nums = *numbers.sort
-  length = sorted_nums.length
   suspect_count = Hash.new
-  sorted_nums.each do |suspect|
-    suspect_count[suspect] = suspect_count[suspect].to_i + 1
-  end
-  sorted_count = suspect_count.sort_by {| key, value| value}
+  # Count number of times integers occurs in list and tally in hash
+  numbers.each { |suspect| suspect_count[suspect] = suspect_count[suspect].to_i + 1 }
+  # Integers placed into array of arrays ordered by count 
+  sorted_count = suspect_count.sort_by {|key, value| value}
   if sorted_count.size == 1
     return sorted_count[0][0]
   elsif sorted_count.size == 0
     return nil
   elsif sorted_count[0][1] < sorted_count[1][1]
+    # Returns integer with the fewest instances
     return sorted_count[0][0]
   else
+    # If all integers have same count return nil
     return nil
   end
 end
+
+# outputs 777
+puts find_suspect(777,3,4,4,5,5,6,6,99,99,3)
+# outputs nil
+puts find_suspect(33,44)
+# outputs nil
+puts find_suspect()

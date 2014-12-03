aa499567ae0c897c9085fb1be3510a4e7be50b8b
Jason Mahony <jason@verticalresponse.com>
2014-12-02 20:27:54 -0800

    working on incomplete lcd display

--- lcd.rb
+++ lcd.rb
@@ -3,37 +3,38 @@
 class Display
   def initialize(integer, scale, lcds)
     @integer = integer
-    @scale = scale.to_i
+    @scale = scale
     @lcds = lcds
   end
   
   def showargs
     puts "  The integer is #{@integer} with a scale of #{@scale}  ".center(60,'*')
   end
-
+  
   # Display the LCD integer
   def display
-#    int_array = @integer.chars.map(&:to_i)
     digits = @integer.chars.map { |n| @lcds[n.to_i] }
-      digits.each do |digit|
-        digit.each do |row|
-          # Scale horizontally
-          row[1, 1] = row[1, 1] * @scale
-          # Scale vertically    
-          if row =~ /\|/
-            @scale.times { puts row }
-          else
-            puts row
-          end
+    digits.each do |digit|
+      digit.each do |row|
+        # Scale horizontally
+        row[1, 1] = row[1, 1] * @scale
+      end
+      digit.each do |row|
+        # Scale vertically    
+        if row =~ /\|/
+          @scale.times { puts row }
+        else
+          puts row
         end
       end
+    end
   end
 end
 
 # Fetching the arguments
 if ARGV.size == 2
   num = ARGV[0]
-  scale = ARGV[1]
+  scale = ARGV[1].to_i
 elsif ARGV.size == 1
   scale = 1
   num = ARGV[0]
@@ -48,15 +49,15 @@ template = [
 
 [' - ',
 '| |',
-' ',
+'  ',
 '| |',
 ' - '],
 
-[' ',
+['  ',
 '  |',
-' ',
+'  ',
 '  |',
-' '],
+'  '],
 
 [' - ',
 '  |',
@@ -65,16 +66,16 @@ template = [
 ' - '],
 
 [' - ',
-'|',
+'| ',
 ' - ',
-'|',
+'| ',
 ' - '],
 
-[' ',
+['  ',
 '| |',
 ' - ',
 '  |',
-' '],
+'  '],
 
 [' - ',
 '| ',
@@ -90,7 +91,7 @@ template = [
 
 [' - ',
 '  |',
-'   ',
+'  ',
 '  |',
 ' '],
 

12e5f5e81ed4bb27c9d2bd9037b7c878e4cbdb01
Jason Mahony <jason@verticalresponse.com>
2014-12-04 13:53:30 -0800

    added comments for sequence script

--- sequence.rb
+++ sequence.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 
+# Find the greatest sequence of 5 digits in an integer string
 def solution(digits)
-#  return nil if digits.length < 5 || digits.to_i == 0
+  # Return nothing if input has less than 5 digits or it equals 0
+  return nil if digits.length < 5 || digits.to_i == 0
   digits.chars.map { |c| digits[digits.index(c), 5].to_i }.max
 end
 
-puts solution("23456987654")
+puts solution("234506984")
 puts solution("345")
 puts solution("00" * 10000)
 puts solution("678" * 99)
-puts solution("6789999")
+puts solution("6789999" * 10000)

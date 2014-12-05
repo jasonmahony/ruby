7ee8aa581ebcd60acae63a4a131f819be907c07c
Jason Mahony <jason@verticalresponse.com>
2014-12-05 15:51:19 -0800

    refactoring cap_me method

--- cap_me.rb
+++ cap_me.rb
@@ -2,11 +2,7 @@
 
 # Accepts array of names, returns array capitalized
 def cap_me(name_array)
-  new_names = Array.new
-  name_array.each do |name|
-    new_names.push(name.downcase.capitalize)
-  end
-  new_names
+  name_array.map { |name| name.capitalize }
 end
 
 name_array = ['jo', 'nelson', 'jurie', 'JASON']

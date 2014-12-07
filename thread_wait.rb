c0f6db08d247349fa42daba9972ba31fedc8e82e
Jason Mahony <jason@verticalresponse.com>
2014-12-06 17:25:25 -0800

    commented thread_wait method

--- thread_wait.rb
+++ thread_wait.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 
+# Returns threads that have finishes
 def wait_for_thread(threads)
   return nil if threads == []
+  # Wait for thread to finish before returning thread
   loop {threads.map {|t| return t unless t.alive?}}
 end
 
 threads = [ Thread.new { 0 } ]
 sleep 1
-wait_for_thread(threads)
+puts wait_for_thread(threads)
